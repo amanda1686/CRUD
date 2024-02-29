c6523e892dd7c1cf8772cf7903afdc62f631a4d0
amanda1686 <80174591+amanda1686@users.noreply.github.com>
2024-02-29 10:29:19 +0100

    middleware

--- Server/Databse/db.js
+++ Server/Databse/db.js
@@ -10,3 +10,5 @@ export  const db = async () =>{
         
     }
 }
+
+export default db
--- Server/routes/AuthRoutes.js
+++ Server/routes/AuthRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import  Register, { Login }  from "../controllers/AuthController.js";
+import { authenticate } from "../controllers/AuthController.js"; // Importar el middleware de autenticación
+import { Register, Login } from "../controllers/AuthController.js";
 
 const router = express.Router();
-router.post("/register", Register)
-router.post("/login", Login)
+router.post("/register", Register);
+router.post("/login", authenticate, Login); // Añadir el middleware de autenticación aquí
 
 export default router;
+
